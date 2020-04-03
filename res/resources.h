df946aae300d165bda57521aa65491dac2f9ee33
DocDooom <kidsonictrading@gmail.com>
2020-04-03 06:05:07 +0100

    adding resource and rom files
    playable rom can be in out\rom.bin

---
+++ res/resources.h
@@ -0,0 +1,9 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const SpriteDefinition imgball;
+extern const SpriteDefinition paddle;
+extern const Image bgtile;
+extern const Image bgtile2;
+
+#endif // _RES_RESOURCES_H_
